8690b4488b2fb7181d5de59b8042dd75f8918396
johnaish <jmaish819@gmail.com>
2025-10-29 12:05:17 +0300

    Added Render domain to ALLOWED_HOSTS

--- kamau_shop/settings.py
+++ kamau_shop/settings.py
@@ -5,7 +5,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-demo-key'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost",
+    "127.0.0.1",
+    "ecommerce-website-h14q.onrender.com"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
